cebc0570ca69a9ac3df099b8ac97997de0da6a34
Toure <toure@redhat.com>
2016-10-12 22:47:26 -0400

    added comments.

--- infrared/main.py
+++ infrared/main.py
@@ -4,9 +4,10 @@ from infrared.cli import core_args
 from infrared.configs import yaml_list
 from infrared.configs import yaml_merge
 
-
+# Merge all spec file which were installed.
 combined_conf = yaml_merge(yaml_list())
 
+# Collect all functions which will correspond to the cli args.
 clg.TYPES.update({function_name: function
                   for function_name, function in vars(core_args).items()
                   if not function_name.startswith('_')})
